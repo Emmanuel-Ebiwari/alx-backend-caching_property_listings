941251bbc360e8f89e761b6f8a7229972a07a6a9
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2025-07-12 19:57:55 +0100

    feat: retrieve and analyze redis cache hit or miss metrics

--- properties/utils.py
+++ properties/utils.py
@@ -1,5 +1,9 @@
 from django.core.cache import cache
 from .models import Property
+from django_redis import get_redis_connection
+import logging
+
+logger = logging.getLogger(__name__)
 
 def get_all_properties():
     """
@@ -12,3 +16,27 @@ def get_all_properties():
         cache.set('all_properties', properties, timeout=3600)
 
     return properties
+
+def get_redis_cache_metrics():
+    """
+    Retrieve and analyze Redis cache
+    hit or miss metrics.
+    """
+    conn = get_redis_connection("default")
+    info = conn.info()
+
+    hits = info.get("keyspace_hits", 0)
+    misses = info.get("keyspace_misses", 0)
+
+    total = hits + misses
+    hit_ratio = (hits / total) if total > 0 else None
+
+    metrics = {
+        "keyspace_hits": hits,
+        "keyspace_misses": misses,
+        "hit_ratio": hit_ratio,
+    }
+
+    logger.info(f"Redis Cache Metrics: {metrics}")
+    return metrics
+
--- properties/views.py
+++ properties/views.py
@@ -1,9 +1,10 @@
 from django.views.decorators.cache import cache_page
 from django.http import JsonResponse
-from .models import Property
 from .utils import get_all_properties
+from django.views.decorators.http import require_GET
 
 @cache_page(60 * 15)
+@require_GET
 def property_list(request):
-    properties = get_all_properties()
+    properties = list(get_all_properties().values())
     return JsonResponse({'data': properties}, safe=False)
