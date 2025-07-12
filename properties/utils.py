13fb8d970def9a17f6e7d3f41b3a8a0dca495ff5
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2025-07-12 18:29:59 +0100

    feat: cache the property queryset in redis for 1 hour using django’s low-level cache api

---
+++ properties/utils.py
@@ -0,0 +1,14 @@
+from django.core.cache import cache
+from .models import Property
+
+def get_all_properties():
+    """
+    Fetch all properties from Redis cache.
+    Cache it for 1 hour if not found.
+    """
+    properties = cache.get('all_properties')
+    if properties is None:
+        properties = list(Property.objects.values())
+        cache.set('all_properties', properties, timeout=3600)
+
+    return properties
